4c32fabcf5f5ed505915311f0aa82230c27d8588
dhruvpujari8971 <dhruvpujari1@gmail.com>
2025-01-29 14:43:36 +0530

    Add files via upload

---
+++ problems.py
@@ -0,0 +1,26 @@
+
+# holding the first and last value of a list
+
+a=[7,3,9,1,4,2]
+target=(7,2)
+
+def calc():
+
+   first_value=(a[0])
+   sum=-1
+   b=len(a)
+   s=sum+b
+   last_value=(a[s])
+   print(f'{first_value},{last_value}')
+calc()
+   
+
+
+
+
+
+
+
+   
+   
+   
